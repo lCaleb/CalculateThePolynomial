fc2f8644c4e70580f7d2a059a8654ab0abe1e353
caleb <lcaleblhenao@gmail.com>
2019-02-16 14:41:03 -0500

    Interfaz del proyecto

--- CalculateThePolynomial/src/interfaz/CalculatorController.java
+++ CalculateThePolynomial/src/interfaz/CalculatorController.java
@@ -1,5 +1,64 @@
 package interfaz;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SplitMenuButton;
+import javafx.scene.layout.GridPane;
+
+
 public class CalculatorController {
 	
+
+
+	
+
+		@FXML private Button generarAleatorio;
+		@FXML private Button generarPolinomio;
+		@FXML private SplitMenuButton seleccionGrado;
+		
+		@FXML private MenuItem uno;
+		@FXML private MenuItem dos;
+		@FXML private MenuItem tres;
+		@FXML private MenuItem cuatro;
+		@FXML private MenuItem cinco;
+		@FXML private MenuItem seis;
+		@FXML private MenuItem siete;
+		@FXML private MenuItem ocho;
+		@FXML private MenuItem nueve;
+		@FXML private MenuItem diez;
+
+		@FXML private GridPane polinomio;
+		
+		
+		public CalculatorController() {
+			
+		}
+		
+		public void initialize() {
+			
+		}
+		
+		
+		public void generarAleatorio() {}
+		public void generarPolinomio() {}
+		public void seleccionGrado() {}
+		public void polinomioEnElGrid() {}
+		public void uno() {}
+		public void dos() {}
+		public void tres() {}
+		public void cuatro() {}
+		public void cinco() {}
+		public void seis() {}
+		public void siete() {}
+		public void ocho() {}
+		public void nueve() {}
+		public void diez() {}
+		
+		
+		
+		
+		
+	
+
 }
